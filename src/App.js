995672ffddfc48307593645fb16a5e2db0ef117e
Michael Prather <mikeprather20@gmail.com>
2022-08-16 14:58:56 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Button from '@mui/material/Button'
+import Feed from './Feed'
+import Contact from './Contact'
+import Home from './Home'
+import Nav from './Nav'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      users: {},
+      name: 'Mikey',
+      age: 34
+    }
+  }
+
+
+  addToAge = ()=>{
+    this.setState({age: this.state.age + 1})
+  }
+
+
+  render() {
+    return (
+
+      <BrowserRouter>
+
+      <div className='main'>
+
+        <Nav />
+        {this.state.name}
+        {this.state.age}
+        <Button variant="contained" onClick={this.addToAge}>Add to age</Button>
+
+        <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/feed' element={<Feed/>}/>
+        </Routes>
+
+      </div>
+
+      </BrowserRouter>
+
+    )
+  }
 }
-
-export default App;
